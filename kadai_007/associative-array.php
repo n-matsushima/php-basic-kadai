bc59b968a31736305a0a8798103e3ada3b3cb81e
n-matsushima <naostacular@gmail.com>
2024-03-06 15:58:00 +0900

    kadai_007初回提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>課題007</title>
+</head>
+<body>
+  <p>
+    <?php
+      $food = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+      print_r($food);
+    ?>
+  </p>
+</body>
+</html>
